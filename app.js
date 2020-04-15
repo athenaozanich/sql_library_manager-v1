fd2f9573b57923ccc02afaaa1b40fd83e8ff070b
athenaozanich <athenaozanich@gmail.com>
2020-04-15 13:34:36 -0400

    refactor code and clean up files

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-const routes = require('./routes/index');
-const books = require('./routes/books');
+const routes = require('./routes/index')
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/books', books);
-
-// catch 404 and forward to error handler
-app.use( (req, res, next) => {
-  next(createError(404));
-});
+app.use('/', routes)
 
 // error handler
-app.use( (err, req, res, next) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  console.log(err.message)
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('books/errors')
+  next(err)
+})
 
-module.exports = app;
+app.use((err, req, res, next) => {
+  res.status(err.status || 404)
+  res.render('books/page_not_found')
+})
+
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,88 @@
 const express = require('express')
 const router = express.Router()
-
+const Book = require('../models').Book
+/* Handler function to wrap each route. */
+function asyncHandler (cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  }
+}
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', asyncHandler(async (req, res, next) => {
   res.redirect('/books')
-})
+}))
+
+/* GET books listing. */
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll(/* { order: [['year', 'ASC']] } */)// commenting out my preferred ordering until after grading
+  res.render('index', { books, title: 'Library Inventory Manager' })
+}))
+
+/* Create a new book form. */
+router.get('/books/new', asyncHandler(async (req, res) => {
+  res.render('books/new_book', { book: {}, title: 'New Book' })
+}))
+
+/* POST create book. */
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book
+  try {
+    book = await Book.create(req.body)
+    res.redirect('/books')// redirect after book entry creation
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') { // checking the error
+      book = await Book.build(req.body)
+      res.render('books/new_book', { book, errors: error.errors, title: 'New Book' })
+    } else {
+      throw error // error caught in the asyncHandler's catch block
+    }
+  }
+}))
+
+/* Edit book form. */
+router.get('/books/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id)
+  if (book) {
+    res.render('books/update_book', { book, title: 'Edit Book' })
+  } else {
+    res.sendStatus(404)
+  }
+}))
+
+/* Update an book. */
+router.post('/books/:id', asyncHandler(async (req, res) => {
+  let book
+  try {
+    book = await Book.findByPk(req.params.id)
+    if (book) {
+      await book.update(req.body)
+      res.redirect('/books')// Redirect after updating book entry
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      book = await Book.build(req.body)
+      book.id = req.params.id // Make sure correct book gets updated
+      res.render('books/update_book', { book, errors: error.errors, title: 'Edit Book' })
+    } else {
+      throw error
+    }
+  }
+}))
+/* Delete individual book. */
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id)
+  if (book) {
+    await book.destroy()
+    res.redirect('/books')// Redirect after deleting book entry
+  } else {
+    res.sendStatus(404)
+  }
+}))
 
 module.exports = router
