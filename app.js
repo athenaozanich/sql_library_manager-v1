6e9e4309e428345af56bd61a1438fb1fb77a61ab
athenaozanich <athenaozanich@gmail.com>
2020-04-23 12:38:22 -0400

    Add pagination and search filtering

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const routes = require('./routes/index')
+const routes = require('./routes/index');
 
-const app = express()
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes)
+app.use('/', routes);
 
 // error handler
 app.use((err, req, res, next) => {
   // render the error page
-  res.status(err.status || 500)
-  res.render('books/errors')
-  console.log('500 handler called')
-})
+  res.status(err.status || 500);
+  res.render('books/errors');
+  console.log('500 handler called');
+});
 
 app.use((req, res, next) => {
-  res.render('books/page_not_found')
-  console.log('404 handler called')
-})
-module.exports = app
+  res.render('books/page_not_found');
+  console.log('404 handler called');
+});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
-const Book = require('../models').Book
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const Book = require('../models').Book;
 /* Handler function to wrap each route. */
 function asyncHandler (cb) {
   return async (req, res, next) => {
@@ -14,90 +14,113 @@ function asyncHandler (cb) {
     }
   }
 }
+const paginate = (page, pageSize) => {
+  const offset = page * pageSize;
+  const limit = pageSize;
+
+  return {
+    offset,
+    limit
+  };
+}
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
-  res.redirect('/books')
-}))
+  res.redirect('/books/pg/1/');
+}));
 
 /* GET books listing. */
-router.get('/books/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll(/* { order: [['year', 'ASC']] } */)// commenting out my preferred ordering until after grading
-  res.render('index', { books, title: 'Library Inventory Manager' })
-}))
+router.get('/books/pg/:pgNum/', asyncHandler(async (req, res) => {
+  const currPg = req.params.pgNum || 1;
+  const bksPrPg = 5;
+  const { count, rows } = await Book.findAndCountAll({
+    ...paginate(req.params.pgNum -1, bksPrPg)
+  });
+  const numOfPgs = Math.ceil(count / bksPrPg);
+  const books = rows;
+  const routePath = "/books/pg/";
+  console.log(currPg);
+  res.render('index', { routePath, currPg, numOfPgs, books, title: 'Library Inventory Manager' })
+}));
 
-router.get('/books/:bkQry/', asyncHandler(async (req, res) => {
-  console.log(req.query.name)
-  const books = await Book.findAll({
+router.get('/books/results/pg/:pgNum/', asyncHandler(async (req, res) => {
+  const currPg = req.params.pgNum || 1;
+  const bksPrPg = 5;
+  const requestedQry = { [Op.like]: `%${req.query.query}%` }
+  const { count, rows } = await Book.findAndCountAll({
     where: {
       [Op.or]: [
         {
-          title: { [Op.like]: `%${req.params.bkQry}%` }
+          title: requestedQry
         },
         {
-          author: { [Op.like]: `%${req.params.bkQry}%` }
+          author: requestedQry
         },
         {
-          genre: { [Op.like]: `%${req.params.bkQry}%` }
+          genre: requestedQry
         },
         {
-          year: { [Op.like]: `%${req.params.bkQry}%` }
+          year: requestedQry
         }
       ]
-    }
-  })
-  res.render('index', { books })
-}))
+    },
+    ...paginate(req.params.pgNum -1 , bksPrPg)
+  });
+  const numOfPgs = Math.ceil(count / bksPrPg);
+  const books = rows;
+  const routePath = "/books/results/pg/";
+  res.render('index', { qry: req.query.query, routePath, currPg, numOfPgs, books, title: 'Library Inventory Manager' });
+}));
 
 /* Create a new book form. */
 router.get('/books/new/', asyncHandler(async (req, res) => {
-  res.render('books/new_book', { book: {}, title: 'New Book' })
-}))
+  res.render('books/new_book', { book: {}, title: 'New Book' });
+}));
 
 /* POST create book. */
 router.post('/books/new/', asyncHandler(async (req, res) => {
-  let book
+  let book;
   try {
-    book = await Book.create(req.body)
-    res.redirect('/books/')// redirect after book entry creation
+    book = await Book.create(req.body);
+    res.redirect('/books/pg/1/');// redirect after book entry creation
   } catch (error) {
     if (error.name === 'SequelizeValidationError') { // checking the error
-      book = await Book.build(req.body)
-      res.render('books/new_book', { book, errors: error.errors, title: 'New Book' })
+      book = await Book.build(req.body);
+      res.render('books/new_book', { book, errors: error.errors, title: 'New Book' });
     } else {
-      throw error // error caught in the asyncHandler's catch block
+      throw error; // error caught in the asyncHandler's catch block
     }
   }
-}))
+}));
 
 /* Edit book form. */
 router.get('/books/:id/', asyncHandler(async (req, res) => {
-  const book = await Book.findByPk(req.params.id)
-  res.render('books/update_book', { book, title: 'Edit Book' })
-}))
+  const book = await Book.findByPk(req.params.id);
+  res.render('books/update_book', { book, title: 'Edit Book' });
+}));
 
 /* Update an book. */
 router.post('/books/:id/', asyncHandler(async (req, res) => {
-  let book
+  let book;
   try {
-    book = await Book.findByPk(req.params.id)
-    await book.update(req.body)
-    res.redirect('/books')// Redirect after updating book entry
+    book = await Book.findByPk(req.params.id);
+    await book.update(req.body);
+    res.redirect('/books/pg/1/');// Redirect after updating book entry
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
-      book = await Book.build(req.body)
-      book.id = req.params.id // Make sure correct book gets updated
-      res.render('books/update_book', { book, errors: error.errors, title: 'Edit Book' })
+      book = await Book.build(req.body);
+      book.id = req.params.id; // Make sure correct book gets updated
+      res.render('books/update_book', { book, errors: error.errors, title: 'Edit Book' });
     } else {
-      throw error
+      throw error;
     }
   }
-}))
+}));
 
 /* Delete individual book. */
 router.post('/books/:id/delete/', asyncHandler(async (req, res) => {
-  const book = await Book.findByPk(req.params.id)
-  await book.destroy()
-  res.redirect('/books')// Redirect after deleting book entry
-}))
+  const book = await Book.findByPk(req.params.id);
+  await book.destroy();
+  res.redirect('/books/pg/1/');// Redirect after deleting book entry
+}));
 
-module.exports = router
+module.exports = router;
